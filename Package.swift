805811580993cbe99febf8ee9d9a8fc475261d24
Vyacheslav Razumeenko <dnc.slava.razumeenko@gmail.com>
2025-01-31 00:35:29 +0200

    feature: package inited

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ToastsUI",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "ToastsUI",
+            targets: ["ToastsUI"]
+        ),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/sunghyun-k/swiftui-window-overlay.git", from: "1.0.0"),
+      .package(url: "https://github.com/pointfreeco/swift-identified-collections?tab=readme-ov-file", from: "1.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "ToastsUI",
+            dependencies: [
+              .product(name: "WindowOverlay", package: "swiftui-window-overlay"),
+              .product(name: "IdentifiedCollections", package: "swift-identified-collections?tab=readme-ov-file")
+            ]
+        ),
+        .testTarget(
+            name: "ToastsUITests",
+            dependencies: ["ToastsUI"]
+        ),
+    ],
+    swiftLanguageModes: [.v5]
+)
